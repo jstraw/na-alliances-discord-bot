44c2eb237ddf6cf02d9def7ebaef4ace731f6107
Jason Straw <jason.straw@gmail.com>
2025-09-30 21:35:41 -0500

    Fix logging, remove extra whitespace

--- na_alliances_discord_bot/channels.py
+++ na_alliances_discord_bot/channels.py
@@ -28,24 +28,25 @@ class ManageChannels(commands.Cog):
                 continue
             for channel in await guild.fetch_channels():
                 if any(x for x in channel_filter if x in channel.name):
-                    log.debug(f"Regenerate {channel.name}")
+                    log.debug(f"Work on {channel.name}")
                     channels.append(channel)
                     categories.add(channel.category)
         for channel in channels:
             if regenerate:
                 log.debug(f"regenerating {channel.name}")
                 await channel.edit(name=f"{channel.name}-rename")
-                await channel.clone(name=channel.name, category=channel.category, 
+                await channel.clone(name=channel.name, category=channel.category,
                                     reason="Rebuilding Team Channels")
                 await channel.delete()
         for category in list(categories):
+            log.debug(f"{category.name} - {[x.name for x in channel.guiild.roles]}")
             role = discord.utils.get(channel.guild.roles, name=category.name)
             for cat_channel in category.channels:
                 if any(x for x in channel_filter if x in cat_channel.name):
                     log.info(f"Change perms on {cat_channel.name} for {role.name}")
                     await cat_channel.set_permissions(role, overwrite=perms)
         return channels
-    
+
     @discord.app_commands.command(name="lock_team_channels", description="Lock Out NA Alliance Server Team Channels")
     @discord.app_commands.check(util.has_role)
     async def lock_team_channels(self, interaction: discord.Interaction):
@@ -62,7 +63,7 @@ class ManageChannels(commands.Cog):
         channels = await self.manage_team_channels(channel_filter, perms, regenerate=True)
         log.info("Locking Team Channels Complete")
         await interaction.edit_original_response(content="Locking Team Channels... Complete")
-        
+
     @discord.app_commands.command(name="lock_reset_channels", description="Lock NA Alliance Reset Channels for all teams")
     @discord.app_commands.check(util.has_role)
     async def lock_reset_channels(self, interaction: discord.Interaction):
@@ -75,7 +76,7 @@ class ManageChannels(commands.Cog):
         channels = await self.manage_team_channels(channel_filter, perms)
         log.info("Locking Reset Channels Complete")
         await interaction.edit_original_response(content="Locking Reset Channels... Complete")
-        
+
 
     @discord.app_commands.command(name="unlock_team_channels", description="Unlock NA Alliance Team Channels (including Reset)")
     @discord.app_commands.check(util.has_role)
